1bc17171036b67903a83dce2655964c9019c4158
Charles Sprayberry <me@cspray.io>
2020-04-13 21:26:11 -0400

    implement ConfigurationBuilder. fixes #7

--- src/ConfigurationBuilder.php
+++ src/ConfigurationBuilder.php
@@ -2,43 +2,122 @@
 
 namespace Cspray\Labrador\Http\Cors;
 
+use InvalidArgumentException;
+
 class ConfigurationBuilder {
 
-    private function __construct(string $origin) {
+    private $origins;
+    private $methods;
+    private $allowedHeaders = [];
+    private $exposableHeaders = [];
+    private $maxAge;
+    private $allowCredentials = false;
+
+    private function __construct(array $origins) {
+        $this->origins = $origins;
     }
 
-    public static function forOrigin(string $origin) : ConfigurationBuilder {
-        return new ConfigurationBuilder($origin);
+    public static function forOrigins(string ...$origins) : ConfigurationBuilder {
+        if (empty($origins)) {
+            $msg = 'At least one Origin must be provided when building a Configuration';
+            throw new InvalidArgumentException($msg);
+        }
+        return new ConfigurationBuilder($origins);
     }
 
     public function withMaxAge(int $maxAge) : self {
+        $this->maxAge = $maxAge;
         return $this;
     }
 
     public function allowMethods(string ...$httpMethods) : self {
+        $this->methods = $httpMethods;
         return $this;
     }
 
     public function allowRequestHeaders(string ...$headerNames) : self {
+        $this->allowedHeaders = $headerNames;
         return $this;
     }
 
     public function exposeResponseHeaders(string ...$headerNames) : self {
+        $this->exposableHeaders = $headerNames;
         return $this;
     }
 
     public function doAllowCredentials() : self {
+        $this->allowCredentials = true;
         return $this;
     }
 
     public function doNotAllowCredentials() : self {
+        $this->allowCredentials = false;
         return $this;
     }
 
     public function build() : Configuration {
-        // Should either reuse ArrayConfiguration or return an anonymous class.
+        if (empty($this->methods)) {
+            throw new InvalidArgumentException('At least one HTTP method must be provided when building a Configuration');
+        }
+        return new class($this->origins, $this->methods, $this->maxAge, $this->allowedHeaders, $this->exposableHeaders, $this->allowCredentials) implements Configuration {
+            private $origins;
+            private $methods;
+            private $maxAge;
+            private $allowedHeaders;
+            private $exposableHeaders;
+            private $allowCredentials;
+
+            public function __construct(array $origins, array $methods, ?int $maxAge, array $allowedHeaders, array $exposableHeaders, bool $allowCredentials) {
+                $this->origins = $origins;
+                $this->methods = $methods;
+                $this->maxAge = $maxAge;
+                $this->allowedHeaders = $allowedHeaders;
+                $this->exposableHeaders = $exposableHeaders;
+                $this->allowCredentials = $allowCredentials;
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getOrigins(): array {
+                return $this->origins;
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getMaxAge(): ?int {
+                return $this->maxAge;
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getAllowedMethods(): array {
+                return $this->methods;
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getAllowedHeaders(): array {
+                return $this->allowedHeaders;
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getExposableHeaders(): array {
+                return $this->exposableHeaders;
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function shouldAllowCredentials(): bool {
+                return $this->allowCredentials;
+            }
+        };
     }
-
-
 
 }
